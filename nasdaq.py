bc69ae173f89bbd8b55c29f06d0cd0799c7f8695
Max Roesler <MaxRoesler@149-160-143-86.dhcp-bl.indiana.edu>
2021-03-25 20:30:09 -0400

    flask working

--- nasdaq.py
+++ nasdaq.py
@@ -1,19 +1,70 @@
 # I used this source to help me find a huge NASDAQ csv file containing stock names: https://towardsdatascience.com/downloading-historical-stock-prices-in-python-93f85f059c1f
 # Market categories feature in CSV file: Q = NASDAQ Global Select MarketSM; G = NASDAQ Global MarketSM; S = NASDAQ Capital Market
 # Round lot size feature: securities to be traded on exchange (typically 100)
-#pip install --upgrade yfinance
+
 import yfinance as yf # Yahoo finance will provide historical stock prices to use for the predictions
 import datetime
 import time
 import requests
 import io
 import pandas as pd
-import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 from joblib import dump, load 
+from sklearn.preprocessing import StandardScaler
+
+def collectDataframe(start, end):
+  # List of stocks in NASDAQ found from towardsdatascience.com to have a collection of stocks to form a model
+  url="https://pkgstore.datahub.io/core/nasdaq-listings/nasdaq-listed_csv/data/7665719fb51081ba0bd834fde71ce822/nasdaq-listed_csv.csv"
+  s = requests.get(url).content
+  companies = pd.read_csv(io.StringIO(s.decode('utf-8')))
+  Symbols = companies['Symbol'].tolist() #create list for the various companies
+  sty = int(start[0])
+  stm = int(start[1])
+  std = int(start[2])
+  ey = int(end[0])
+  em = int(end[1])
+  ed = int(end[2])
+  if (sty > ey):
+    print("Start year must be lower than end year")
+    return
+  if (sty == ey):
+    if (stm > em):
+      print("End month has to be after start month")
+      return
+    if (stm == em):
+      if (std > ed):
+        print("End day has to be after start day")
+        return
+  start = datetime.datetime(sty,stm,std) # Start and end time to look at stock history (1 month from november-december 2020 in this model)
+  end = datetime.datetime(ey,em,ed)
+# use arguments to find date 
+# This creates an empty list
+
+  stock_final = pd.DataFrame()
+# Go through every symbol that we added to the list
+  for i in Symbols:  
+    
+    # This prints each symbol that is being acquired for
+    print( str(Symbols.index(i)) + str(' : ') + i, sep=',', end=',', flush=True)  
+    
+    try:
+        # Yahoo Finance - finds and collects the information needed to complete the dataframe and list for predictions
+        stock = []
+        stock = yf.download(i,start=start, end=end, progress=False)
+        
+        # This appends the individual stock prices to the list
+        if len(stock) == 0:
+            None
+        else:
+            stock['Name']=i
+            stock_final = stock_final.append(stock,sort=False)
+    except Exception:
+        None
+  dump(stock_final, 'ticker.pkl')
+  return stock_final
 
 def timeframe(start, end):
   sty = int(start[0])
@@ -39,6 +90,9 @@ def timeframe(start, end):
   dump(end,'end.pkl')
   return start, end
 
+#stock_final = timeframe(start, end)
+#timeframe(start,end)
+
 def nasdaq(ticker, days):
   stock_final = load('ticker.pkl')
   start = load('start.pkl')
@@ -53,11 +107,11 @@ def nasdaq(ticker, days):
   #st = stock_final.query("Name == '{}'".format(ticker))
   st = yf.download(ticker,start=start, end=end, progress=False)
   #st = st.drop(columns=['Name'])
-  st.plot(y='Adj Close') 
-  plt.title('Date V. Actual Adj. Closing Price')
-  plt.xlabel('Date (Days)')
-  plt.ylabel('Adj. Closing Price ($)')
-  plt.show()  
+  #st.plot(y='Adj Close') 
+  #plt.title('Date V. Actual Adj. Closing Price')
+  #plt.xlabel('Date (Days)')
+  #plt.ylabel('Adj. Closing Price ($)')
+  #plt.show()  
 
   #st = my_tick('AAPL')
 
@@ -110,19 +164,15 @@ def nasdaq(ticker, days):
     if (y_pred[i] < y_pred[i+1]):
       max = y_pred[i+1]
       day = i+2 # for the accurate number of days and not the array number
-  print("The highest predicted price in", days, "days is day", day, "with a predicted price of", max)
+  print("The highest predicted price in", days, "days is day", day, f"with a predicted price of ${round(max, 2)}")
   if (day==0):
     print("This means that the stock price is predicted to go down in the next", days, "days")
 # When day 0 is the lowest, that means that the stock price is predicted to go down
-  plt.title('Support Vector Regression Model: Days V. Predicted Adj. Closing Price')
-  plt.ylabel('Predicted Stock Price Average ($)')
-  plt.xlabel('Time (Days)')
-  plt.ticklabel_format(useOffset=False)
-  plt.plot(y_pred)
-  plt.show()
-  return
+  #plt.title(f'{ticker} SVR Model: Days V. Predicted Adj. Closing Price')
+  #plt.ylabel('Predicted Stock Price Average ($)')
+  #plt.xlabel('Time (Days)')
+  #plt.ticklabel_format(useOffset=False)
+  #plt.plot(y_pred)
+  #plt.show()
+  return y_pred
 
-start = [2020, 12, 1]
-end = [2021, 1, 1]
-timeframe(start, end)
-nasdaq('AMZN', 2)
--- server.py
+++ server.py
@@ -2,23 +2,40 @@ from flask import Flask
 from flask import jsonify
 import connexion
 from joblib import load
+from nasdaq import nasdaq, timeframe
 
 #load the model
 
-stock_final = load('ticker2.pkl')
+#stock_final = load('ticker2.pkl')
 
 # Create the application instance
-app = connexion.App(__name__, specification_dir="./")
+#app = connexion.App(__name__, specification_dir="./")
 
 # Read the yaml file to configure the endpoints
-app.add_api("master.yaml")
+#app.add_api("master.yaml")
 
 # create a URL route in our application for "/"
-@app.route("/")
-def home():
-    msg = {"msg": "It's working!"}
-    return jsonify(msg)
 
+app = Flask(__name__)
+
+@app.route('/timeframe/<start>/<end>')
+def update(start=None, end=None):
+    if not start or not end:
+        return jsonify({"Status" : "Error", "Message" : "Invalid Date Format. Please use the date format as YYYY-MM-DD."})
+    start = [int(s) for s in start.split('-')]
+    end = [int(s) for s in end.split('-')]
+    timeframe(start, end)
+    return jsonify({"Status" : "Success", "Message" : "Timeframe Updated"})
+
+@app.route('/nasdaq/<ticker>/<days>')
+def get(ticker=None, days=None):
+    if not ticker:
+        return jsonify({"Status" : "Error", "Message" : "Invalid Format. Please run localhost/nasdaq/<ticker>/<days>/"})
+    try:
+        pred = nasdaq(ticker, days)
+    except FileNotFoundError:
+        return jsonify({"Status" : "Error", "Message" : "Please run localhost/update/<start>/<end>"})
+    return jsonify({"Status" : "Success", "Ticker" : str(ticker), "Prediction" : str(pred)})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
